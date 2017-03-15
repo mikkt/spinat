e367a7dd373a7283d1f2de22f5a29816b3783863
Mikk-PC\Mikk <mikk.tark@gmail.com>
2017-03-15 11:44:24 +0200

    test.php muudetud

--- application/views/pages/test.php
+++ application/views/pages/test.php
@@ -2,12 +2,17 @@
 
 	$this->db->select('username, email');
 	$this->db->from('user');
+	$this->db->where('username', 'test');
 	$query = $this->db->get();
 	
-	foreach ($query->result() as $row)
+	foreach ($query->result() as $row) 
 	{
 		echo $row->username;
+		echo '<br />';
 		echo $row->email;
+		echo '<br />';
 	}
+	
+	echo '<hr />';
 	var_dump(session_destroy());
 	
