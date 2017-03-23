04c1271575a5935fc58855c671f3eb3ce00a490c
Mikk-PC\Mikk <mikk.tark@gmail.com>
2017-03-23 21:21:06 +0200

    css headerisse, js lõppu

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,7 +2,9 @@
 		<div class="text-center">
 			<p>&copy; 2017 Spinat.</p>
 		</div>
-        </footer>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>media/css/bootstrap.min.css">
+    </footer>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type='text/javascript' src="<?php echo base_url(); ?>media/js/bootstrap.min.js"></script>
 </body>
 </html>
