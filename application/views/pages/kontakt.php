c2f4337dd038ca279502340c779d9ebf08e7a58d
Mikk-PC\Mikk <mikk.tark@gmail.com>
2017-04-23 13:12:38 +0300

    Kontakt leht kasutab contact.xml failist parsetud infot, et kuvada email, telefon ja tiimiliikmete nimed.

--- application/views/pages/kontakt.php
+++ application/views/pages/kontakt.php
@@ -1,15 +1,20 @@
 <div class="container">
 <h2><?php echo $title; ?></h2>
 <div class="panel panel-default">
-    <div class="panel-header"><h4><?php echo $lang_information; ?></h4></div>
+    <div class="panel-heading"><h4><?php echo $lang_information; ?></h4></div>
     <div class="panel-body">
         <p>
-            Email: bla@bla.bla
-        </p>
+		<?php
+			$xml = simplexml_load_file(base_url() . "/media/xml/contact.xml");
+			echo $xml->email . '<br />';
+			echo $xml->phone . '<br />';
+			echo $xml->team . '<br />';
+		?>
+		</p>
     </div>
 </div>
 <div class="panel panel-default">
-    <div class="panel-header"><h4><?php echo $lang_map; ?></h4></div>
+    <div class="panel-heading"><h4><?php echo $lang_map; ?></h4></div>
     <div class="panel-body">
         <?php echo $map['js'];?>
         <?php echo $map['html'];?>
